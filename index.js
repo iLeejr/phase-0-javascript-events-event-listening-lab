b152fa3dca8ef0e7354b92b31d2647b43c6ad4f0
Kaleb Malone <kalebmalone7@gmail.com>
2024-09-08 11:21:41 -0700

    completed test

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+ 
+  function addingEventListener() {
+    const button = document.getElementById('button');
+    if (button) { 
+      button.addEventListener('click', clickAlert);
+    }
+  }
+  
+  addingEventListener();
+  
